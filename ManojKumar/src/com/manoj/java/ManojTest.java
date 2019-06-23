409a001133c5f769cf2a1a98db1330abec93825e
vasudeva <vasu.584@gmail.com>
2019-06-23 10:40:42 +0530

    LatestUpdate

---
+++ ManojKumar/src/com/manoj/java/ManojTest.java
@@ -0,0 +1,14 @@
+package com.manoj.java;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ManojTest {
+
+	public static void main(String[] args) 
+	{
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("Http://gmail.com");
+
+	}
+
+}
